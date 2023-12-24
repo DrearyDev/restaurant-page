d7de4f347c3fb68078ce40788a25ca597086c70d
drearydev <143649090+DrearyDev@users.noreply.github.com>
2023-12-23 19:52:19 -0500

    Create Contact Page

--- src/contact.js
+++ src/contact.js
@@ -1,5 +1,39 @@
+const content = document.querySelector('.content');
+
+
+const createTitle = (() => {
+    let title = document.createElement('div');
+    title.classList.add('title');
+    title.textContent = 'Contact us';
+
+    return title;
+})();
+
+const createContactDetails = (() => {
+    let ul = document.createElement('ul');
+    ul.classList.add('contact-list');
+    ul.innerHTML = `
+    Email: <li>WeAreNotResponsible@protonmail.me</li>
+    <br>
+    Phone Number: <li>911-911-9191</li>
+    <br>
+    Address: <li>3730 SugerCane Crescent, WhinePeg, ON L4T 2Z4, America</li>`;
+
+    return ul;
+})();
+
+const createDisclaimer = (() => {
+    let div = document.createElement('div');
+    div.classList.add('disclaimer');
+    div.textContent = `We are not responsible for any health issues as a result of eating here..`;
+
+    return div;
+})();
+
 function contact() {
-    console.log('contact module log');
+    content.appendChild(createTitle);
+    content.appendChild(createContactDetails);
+    content.appendChild(createDisclaimer);
 };
 
 export { contact };
